6fb20cfdede7276d41ce0595fe85f3a9ba3c4a94
tanjavetter04 <tanja-vetter@gmx.net>
2024-05-21 22:12:22 +0200

    refactor code + openapi

--- src/routes/api/points/+server.ts
+++ src/routes/api/points/+server.ts
@@ -2,60 +2,6 @@ import { redirect, error } from "@sveltejs/kit"
 import type { RequestHandler } from "./$types"
 import { supabase } from "$lib/supabaseClient"
 
-export const POST: RequestHandler = async ({ request, cookies }) => {
-    const user_id = cookies.get('sessionId');
-
-    if (!user_id) {
-        throw error(401, { message: 'You are not logged in' });
-    }
-    
-    const { longitude, latitude } = await request.json();
-
-    const { error: err } = await supabase.from('points').insert([{ longitude: longitude, latitude: latitude, user_id: user_id }]);
-
-    let url = new URL('http://localhost:5173/api/reverseCountry');
-    url.searchParams.append('longitude', longitude);
-    url.searchParams.append('latitude', latitude);
-
-    const response = await fetch(url, {
-        method: 'GET',
-    });
-
-    response.json().then(async function (value) {
-        let country = value.country;
-        let countryUrl = new URL('http://localhost:5173/api/country');
-        countryUrl.searchParams.append('country', country);
-        countryUrl.searchParams.append('user_id', user_id!!)
-
-        const countryResponse = await fetch(countryUrl, {
-			method: 'GET'
-		});
-
-        countryResponse.json().then(async function (point_count) {
-            if (point_count.length != 0) {
-                let newPoint_count = point_count[0].point_count + 1;
-                await fetch('http://localhost:5173/api/country', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ country, newPoint_count, user_id })
-                })
-            } else {
-                await fetch('http://localhost:5173/api/country', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ country, user_id })
-                })
-            }
-        })
-    });
-
-    redirect(303, "/home");
-}
-
 export const GET: RequestHandler = async ({ cookies }) => {
     const user_id = cookies.get('sessionId');
 
@@ -72,6 +18,31 @@ export const GET: RequestHandler = async ({ cookies }) => {
     return new Response(JSON.stringify(data), { status: 200 });
 }
 
+export const POST: RequestHandler = async ({ request, cookies }) => {
+    const user_id = cookies.get('sessionId');
+
+    if (!user_id) {
+        throw error(401, { message: 'You are not logged in' });
+    }
+
+    const { longitude, latitude } = await request.json();
+
+    const { error: err } = await supabase.from('points').insert([{ longitude: longitude, latitude: latitude, user_id: user_id }]);
+
+    const value = await reverseCountry(longitude, latitude);
+    const country = value.country;
+
+    const point_count = await getCountry(country, user_id);
+    if (point_count.length != 0) {
+        let newPoint_count = point_count[0].point_count + 1;
+        putCountry(country, newPoint_count, user_id);
+    } else {
+        postCountry(country, user_id);
+    }
+
+    redirect(303, "/home");
+}
+
 export const DELETE: RequestHandler = async ({ request, cookies }) => {
     const user_id = cookies.get('sessionId');
 
@@ -83,45 +54,68 @@ export const DELETE: RequestHandler = async ({ request, cookies }) => {
 
     const { error: err } = await supabase.from('points').delete().match({ user_id: user_id, longitude: longitude, latitude: latitude });
 
+    const value = await reverseCountry(longitude, latitude);
+    let country = value.country;
+
+    const point_count = await getCountry(country, user_id);
+    if (point_count[0].point_count > 1) {
+        let newPoint_count = point_count[0].point_count - 1;
+        putCountry(country, newPoint_count, user_id);
+    } else {
+        deleteCountry(country, user_id);
+    }
+
+    redirect(303, "/home");
+}
+
+async function reverseCountry(longitude: string, latitude: string) {
     let url = new URL('http://localhost:5173/api/reverseCountry');
     url.searchParams.append('longitude', longitude);
     url.searchParams.append('latitude', latitude);
-
     const response = await fetch(url, {
         method: 'GET',
     });
+    return response.json()
+}
 
-    response.json().then(async function (value) {
-        let country = value.country;
-        let countryUrl = new URL('http://localhost:5173/api/country');
-        countryUrl.searchParams.append('country', country);
-        countryUrl.searchParams.append('user_id', user_id!!);
+async function getCountry(country: string, user_id: string) {
+    const countryUrl = new URL('http://localhost:5173/api/country');
+    countryUrl.searchParams.append('country', country);
+    countryUrl.searchParams.append('user_id', user_id!!)
 
-        const countryResponse = await fetch(countryUrl, {
-			method: 'GET'
-		});
-
-        countryResponse.json().then(async function (point_count) {
-            if (point_count[0].point_count > 1) {
-                let newPoint_count = point_count[0].point_count - 1;
-                await fetch('http://localhost:5173/api/country', {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ country, newPoint_count, user_id })
-                })
-            } else {
-                await fetch('http://localhost:5173/api/country', {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ country, user_id })
-                })
-            }
-        })
+    const countryResponse = await fetch(countryUrl, {
+        method: 'GET'
     });
 
-    redirect(303, "/home");
+    return countryResponse.json()
+}
+
+async function putCountry(country: string, newPoint_count: number, user_id: string) {
+    await fetch('http://localhost:5173/api/country', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ country, newPoint_count, user_id })
+    })
+}
+
+async function postCountry(country: string, user_id: string) {
+    await fetch('http://localhost:5173/api/country', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ country, user_id })
+    })
+}
+
+async function deleteCountry(country: string, user_id: string) {
+    await fetch('http://localhost:5173/api/country', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ country, user_id })
+    })
 }
